83af47a2ec9cc576a1490e9f9f4ebe2f0ac2c014
Isabela Pereira Gregio <136081550+isabelagregio@users.noreply.github.com>
2025-02-10 17:48:12 -0300

    Update fatorial.asm

--- submission/fatorial.asm
+++ submission/fatorial.asm
@@ -1,37 +1,37 @@
 LD N 	;carrega o numero o qual sera calculado o fatorial
-JZ FAT_ZERO ;caso do fatorial de zero 
 MM RES  ;guarda o valor inicial do N em RES
 SC FAT 	;chamada da subrotina
 MM RES 	;armazena o resultado final 
-HM
+HM =0
 
-FA_ZERO 
-	LD UM 
-	MM PROV
-	LD N
+
 
 FAT
-	LD UM  	;comeca com 1
-	MM PROV ;armazena o valor provisorio
-	LD N 	;carrega N
+	LD N 
+	JZ FIM 
 LOOP
-	JZ FIM  ;se for nulo termina
-	ML RES 	;multiplica pelo valor acumulado 
-	MM RES	;atualiza o valor acumulado 
-	SB UM 	;subtrai um 
-	JP LOOP ;chamada do loop
-	
-	
+	MM PROV 
+	ML RES
+	MM RES 
+	LD PROV
+	SB UM 
+	MM PROV
+	JZ FIM 
+	JP LOOP
 FIM 
-	LD PROV ;valor final no acumulador
-	RS FAT ;fim da subrotina
-	
-	HM 
+	RS FAT
 
 ; rotulos 
-N K /0003
-RES K /0001
-UM  K  /0001
-PROV K /0001
+@ /100
+N  K = 3
+
+@ /102
+RES K = 1 ;reserva uma posicao da memoria para armazenar o resultado final 
+
+@ /104 
+PROV K = 1 ;resultado provisorio
+
+@ /106 
+UM K = 1
 
 
